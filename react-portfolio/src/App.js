98adf59d4b7bcfc6eeedff22f557b83420bdd111
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-22 17:45:54 -0500

    updating the MainPage file

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Portfolio from "./components/Portfolio";
-import Resume from "./components/Resume";
-import Footer from "./components/Footer";
-
+import MainPage from "./components/MainPage"
+// import About from "./components/pages/About";
+// import Contact from "./components/pages/Contact";
+// import Navbar from "./components/Navbar";
+// import Portfolio from "./components/pages/Portfolio";
+// import Resume from "./components/pages/Resume";
+// import Footer from "./components/pages/Footer";
 export default function App() {
   return (
     <main>
-      <Navbar />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Resume />
-      <Footer />
+    <MainPage />
     </main>
   );
 }
