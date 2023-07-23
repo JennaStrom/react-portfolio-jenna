1e74c44cbbf1d3282187c8735b471d7746953300
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-23 15:29:11 -0500

    moving some stuff around, updated version for react and add links to footer

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import MainPage from "./components/MainPage"
-// import About from "./components/pages/About";
+import About from "./components/pages/About";
 // import Contact from "./components/pages/Contact";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 // import Portfolio from "./components/pages/Portfolio";
 // import Resume from "./components/pages/Resume";
-// import Footer from "./components/pages/Footer";
+import Footer from "./components/Footer";
+
 export default function App() {
   return (
     <main>
-    <MainPage />
+      <Navbar />
+      {/* <About /> */}
+      <Footer />
     </main>
   );
 }
--- react-portfolio/src/components/pages/Portfolio.jsx
+++ react-portfolio/src/components/pages/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { projects } from "../data";
+import { projects } from "../../data";
 
 export default function Portfolio() {
     <section id="portfolio">
