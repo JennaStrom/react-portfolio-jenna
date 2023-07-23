c020252c8bd033f9690b92be50151a63612071e2
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-23 14:21:10 -0500

    working on the different pages

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,3 +1,34 @@
-// import React from "react";
+import React from "react";
+import { projects } from "../data";
 
-// export default function Portfolio() {}
+export default function Portfolio() {
+    <section id="portfolio">
+        <h1>Portfolio</h1>
+        <p>Checkout the apps I have created below!</p>
+        <div className="flex flex-wrap -m-4">
+            {projects.map((project) => (
+                <a
+                    href={project.link}
+                    key={project.image}
+                    className="">
+                    <div className="">
+                        <img
+                            alt="gallery"
+                            className=""
+                            src={project.image}
+                        />
+                        <div className="">
+                            <h2 className="">
+                                {project.subtitle}
+                            </h2>
+                            <h1 className="">
+                                {project.title}
+                            </h1>
+                            <p className="">{project.description}</p>
+                        </div>
+                    </div>
+                </a>
+            ))}
+        </div>
+    </section>
+}
--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,3 +1,7 @@
-// import React from "react";
+import React from "react";
 
-// export default function Resume() {}
+export default function Resume() {
+    <div>
+    <h1>Resume</h1>
+  </div>
+}
