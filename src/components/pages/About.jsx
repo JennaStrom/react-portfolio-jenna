6cb44754e77d46ec3223616546d42598cb3e1e2b
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-29 18:36:34 -0500

    fixed two photo typos

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                     alt="headshot"
                     className="img-fluid"
                     style={{width: "250px", height: "250px"}}
-                    src="./headshot.jpg"
+                    src="./Headshot.jpg"
                 />
                 <p id="about-me-paragraph" className="pt-5">I recently finished the University of Minnesota Full Stack Coding Boot Camp. I have a background in design and was motivated by my love of solving puzzles to make a career change. I really enjoyed all aspects of web development, but some of my favorites are styling the page and React. I would love to take my over a decade of management experience and work for your company.</p>
             </div>
