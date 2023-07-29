1e89bc5b86a0015bfa824097e6b474a5f10d8a5c
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-29 09:15:38 -0500

    Added some stuff to the resume page and the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,8 +3,15 @@ import React from "react";
 export default function About() {
     return (
         <section id="about" className="container">
-            <h1>Jenna Strom</h1>
             <h2>Full Stack Web Developer</h2>
+            <div className="">
+                <img
+                    alt="headshot"
+                    className="img-fluid"
+                    style={{width: "250px", height: "250px"}}
+                    src="./headshot.jpg"
+                />
+            </div>
         </section>
     )
 }
